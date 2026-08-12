5a7e0d1783bbfb9e27d09a862e45980bec027b93
parmarsujal9118-maker <parmarsujal9118@gmail.com>
2026-08-12 10:09:24 +0530

    Add files via upload

---
+++ 26BCPT354.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h> //header file
+int main()
+{
+    /*program  2 nos.*/
+    int a,b,c;
+    printf("enter two nos.");
+    scanf("%d %d",&a,&b);
+    c = a + b;
+    printf("%d + %d = %d\n",a,b,c);
+    return 0;
+}
