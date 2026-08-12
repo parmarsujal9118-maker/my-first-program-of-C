cfe45e42254530c0c755fe6da4efdbafbefe4c92
parmarsujal9118-maker <parmarsujal9118@gmail.com>
2026-08-12 20:13:00 +0530

    Add files via upload

---
+++ C2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() 
+{
+//a is a radius and b is a area of circle//
+float a,b;
+printf("enter radius of a circle");
+scanf("%f",&a);
+b = 22/7*a*a;
+printf("area of a circle is %.2f",b);
+return 0;
+}
