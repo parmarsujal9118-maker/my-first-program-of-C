fb2e047a3bd11965ba19351662e5709f2982b4e8
parmarsujal9118-maker <parmarsujal9118@gmail.com>
2026-08-15 19:29:53 +0530

    Add files via upload

---
+++ Convert dollars into RS.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+//convert dollars into RS.//
+float dollar,RS;
+printf("Enter value of dollar : ");
+scanf("%f",&dollar);
+printf("RS is : %f\n", dollar * 48);
+return 0;
+}
