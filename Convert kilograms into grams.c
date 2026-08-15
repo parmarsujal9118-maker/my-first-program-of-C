86ecf6e639d7d80c93d3cb719b039b95a823aa46
parmarsujal9118-maker <parmarsujal9118@gmail.com>
2026-08-15 21:01:59 +0530

    Add files via upload

---
+++ Convert kilograms into grams.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+//convert kilograms into grams//
+float grams,kilograms;
+printf("Enter weight in kilograms : ");
+scanf("%f",&kilograms);
+printf("weight in grams is : %f\n",kilograms * 1000);
+return 0;
+}
