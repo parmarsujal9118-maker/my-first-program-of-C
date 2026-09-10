52bcbae2df0a3ee3bf06cd6e0aaef754c7f74c83
parmarsujal9118-maker <parmarsujal9118@gmail.com>
2026-09-10 22:10:01 +0530

    Add files via upload

---
+++ To check if the given number is  a natural number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()  {
+int a;
+printf("Enter a number : ");
+scanf("%d",&a);
+if(a>0){
+printf("This is a natural number");
+}
+else { 
+printf("This is not a natural number");
+}
+
+return 0;
+}
