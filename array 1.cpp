d5842ee81a8aec9dd8c072f008ebc41e5689900c
parmarsujal9118-maker <parmarsujal9118@gmail.com>
2026-09-16 10:34:10 +0530

    Add files via upload

---
+++ array 1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()  {
+ int a[4];
+ printf("Enter 5 values : ");
+ for(int i=0;i<=4;i++){
+    scanf("%d",&a[i]);
+ }
+for(int i=0;i<=4;i++) {
+
+printf("%d\n",a[i]);
+}
+
+return 0;
+          }
