80801ce09244cd0210db9a5ca3c1ae724edffa71
parmarsujal9118-maker <parmarsujal9118@gmail.com>
2026-09-16 15:44:37 +0530

    Add files via upload

---
+++ print sum of 1st n odd numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+int n, sum=0;
+printf("Enter number (n) : ");
+scanf("%d",&n);
+for(int i=0;i<n;i++) 
+{
+sum = sum +(2*i+1);
+}
+printf("%d\n",sum);
+
+return 0;
+}
